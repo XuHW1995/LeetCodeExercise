26ee0ab7557a3bc3345a0ded670409648b07e88e
Mr.X <790543974@qq.com>
2019-11-05 21:56:40 +0800

    快速排序

--- LeetCodeExercise/Program.cs
+++ LeetCodeExercise/Program.cs
@@ -108,9 +108,37 @@ namespace LeetCodeExercise
         #endregion
 
         #region 快速排序 时间复杂度O(nlgn)，不稳定排序
-        static void QuickSort(int[] arrs)
+        public static void QuickSort(int[] arr)
         {
+            if (arr == null || arr.Length == 0)
+                return;
+            quickSort(arr, 0, arr.Length - 1);
+        }
 
+        private static void quickSort(int[] arr, int left, int right)
+        {
+            if (left >= right)
+                return;
+            int pivotPos = partition(arr, left, right);
+            quickSort(arr, left, pivotPos - 1);
+            quickSort(arr, pivotPos + 1, right);
+        }
+
+        private static int partition(int[] arr, int left, int right)
+        {
+            int pivotKey = arr[left];
+
+            while (left < right)
+            {
+                while (left < right && arr[right] >= pivotKey)
+                    right--;
+                arr[left] = arr[right]; //把小的移动到左边
+                while (left < right && arr[left] <= pivotKey)
+                    left++;
+                arr[right] = arr[left]; //把大的移动到右边
+            }
+            arr[left] = pivotKey; //最后把pivot赋值到中间
+            return left;
         }
         #endregion
     }
