4f9bf8b85a74d1fbeaf884eadd333034de683f39
Mr.X <790543974@qq.com>
2020-01-08 18:12:38 +0800

    重复元素

--- LeetCodeExercise/ArrayType.cs
+++ LeetCodeExercise/ArrayType.cs
@@ -135,5 +135,27 @@ namespace LeetCodeExercise
             }
         }
         #endregion
+
+        #region 存在重复
+        /*
+            给定一个整数数组，判断是否存在重复元素。
+            如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+        
+            输入: [1,1,1,3,3,4,3,2,4,2]
+            输出: true
+        */
+        //排序后比较相邻元素
+
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                if (nums[i] == nums[i + 1]) return true;
+            }
+            return false;
+        }
+
+        #endregion
     }
 }
